ac9c26046e726dea635ca29d96a047c0e02ed1d0
ThenamXe0n <thenameet0@gmail.com>
2026-01-09 20:36:31 +0530

    api / delete (hard delete and soft delete) product and update product

--- cart-shop/backend/controllers/Product.controller.js
+++ cart-shop/backend/controllers/Product.controller.js
@@ -1,27 +1,30 @@
 import ProductModel from "../models/product.model.js";
+import ApiResponse from "../utils/apiResponseHandler.js";
 import generateProductCode from "../utils/generateProductCode.js";
 
 export async function addProduct(req, res) {
-  console.log("controller file from multer",req.file)
+  console.log("controller file from multer", req.file);
 
-
-  let filePath =`${req.protocol}://${req.host}/${req.file.destination}/${req.file.filename}`
+  let filePath = `${req.protocol}://${req.host}/${req.file.destination}/${req.file.filename}`;
   try {
     const generatedCode = generateProductCode();
     const product = await ProductModel.create({
       ...req.body,
       productCode: generatedCode,
-      thumbnail:filePath
+      thumbnail: filePath,
     });
-    res.status(201).json({
-      message: "Product posted successfully, wait for approval by admin",
-      data: product,
-    });
+
+    res
+      .status(201)
+      .json(
+        new ApiResponse(
+          true,
+          "Product posted successfully, wait for approval by admin",
+          product
+        )
+      );
   } catch (error) {
-    res.status(500).json({
-      message: error.message,
-      data: null,
-    });
+    res.status(500).json(new ApiResponse(false, error.message));
   }
 }
 
@@ -50,11 +53,55 @@ export async function getAllProducts(req, res) {
   }
 }
 
-
 //update
 
-//delete
+export async function updateProductDetails(req, res) {
+  // let payload = req.body;
+  let id = req.params.id
+  console.log(id)
+  // console.log("parms are",req.params.productId)
 
+  try {
+    let updatedProduct = await ProductModel.findByIdAndUpdate(id, req.body,{new:true});
+    res
+      .status(200)
+      .json(new ApiResponse(true, "product updated!", updatedProduct));
+  } catch (error) {
+    res.status(500).json(new ApiResponse(false, error.message));
+  }
+}
+
+//soft delete
+export async function deleteProductTemp(req, res) {
+  // let payload = req.body;
+  let id = req.params.id
+  // console.log("parms are",req.params.productId)
+
+  try {
+    let deletedProduct = await ProductModel.findByIdAndUpdate(id,{isDeleted:true},{new:true});
+    res
+      .status(200)
+      .json(new ApiResponse(true, "product moved to trash!", deletedProduct));
+  } catch (error) {
+    res.status(500).json(new ApiResponse(false, error.message));
+  }
+}
+//hard delete
+export async function deleteProduct(req, res) {
+  // let payload = req.body;
+  let id = req.params.id
+  console.log(id)
+  // console.log("parms are",req.params.productId)
+
+  try {
+    let deletedProduct = await ProductModel.findByIdAndDelete(id);
+    res
+      .status(200)
+      .json(new ApiResponse(true, "product deleted!", deletedProduct));
+  } catch (error) {
+    res.status(500).json(new ApiResponse(false, error.message));
+  }
+}
 //admin access getall
 
 //admin access update
--- cart-shop/backend/routes/api.routes.js
+++ cart-shop/backend/routes/api.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { loginUser, registerUser } from "../controllers/User.controller.js";
-import { addProduct, getAllProducts } from "../controllers/Product.controller.js";
+import { addProduct, deleteProduct, deleteProductTemp, getAllProducts, updateProductDetails } from "../controllers/Product.controller.js";
 import upload from "../config/multerConfig.js"
 const router = Router();
 
@@ -11,5 +11,10 @@ router.post("/user/login", loginUser);
 //============product routers==========
 router.post("/product/create",upload.single("poster") ,addProduct);
 router.get("/product/getAll",getAllProducts)
+router.patch("/product/update/:id",updateProductDetails)
+router.delete("/product/:id",deleteProduct)
+router.patch("/product/deleteTemp/:id",deleteProductTemp)
+
+// axios.patch("http://localhost:8080/api/product/update/${product.id}",{name:"abc"})
 
 export default router;
